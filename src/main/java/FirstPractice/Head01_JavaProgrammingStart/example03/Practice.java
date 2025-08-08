51cb4d1103d6b231182f12f96728b5ee9e76a4d9
hiingjan <phjung1216@naver.com>
2025-08-08 14:13:08 +0900

    ff

--- src/main/java/FirstPractice/Head01_JavaProgrammingStart/example03/Practice.java
+++ src/main/java/FirstPractice/Head01_JavaProgrammingStart/example03/Practice.java
@@ -1,4 +1,14 @@
 package FirstPractice.Head01_JavaProgrammingStart.example03;
 
 public class Practice {
+    public static void main(String[] args) {
+
+        int x = 20, y = 1000;
+
+        System.out.println("합: " + (x + y));
+        System.out.println("차: " + (x - y));
+        System.out.println("곱: " + (x * y));
+        System.out.println("몫: " + (x / y));
+        System.out.println("나머지: " + (x % y));
+    }
 }
---
+++ src/main/java/FirstPractice/Head01_JavaProgrammingStart/example04/Practice.java
@@ -0,0 +1,9 @@
+package FirstPractice.Head01_JavaProgrammingStart.example04;
+
+public class Practice {
+    public static void main(String[] args) {
+        int score = 32;
+        String result = (score >= 80) ? "C학점" : "D학점";
+        System.out.println("성적: " + result);
+    }
+}
