5ae620e3bdef48d764a36fce694ae56b7a27c563
hiingjan <phjung1216@naver.com>
2025-08-08 15:24:03 +0900

    haha

---
+++ src/main/java/FirstPractice/Head01_JavaProgrammingStart/example08/Practice.java
@@ -0,0 +1,16 @@
+package FirstPractice.Head01_JavaProgrammingStart.example08;
+
+public class Practice {
+    public static void main(String[] args){
+        for (int i = 3; i <= 10; i++) {
+            System.out.println("i = " + i);
+        }
+
+        int count = 4;
+        while (count < 10) {
+            System.out.println("count = " + count);
+            count++;
+        }
+
+    }
+}
