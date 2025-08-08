982fc9ab2d1523a85c1c1e1eaf12f0bd56c9c49e
hiingjan <phjung1216@naver.com>
2025-08-08 14:49:07 +0900

    d

---
+++ src/main/java/FirstPractice/Head01_JavaProgrammingStart/example06/Practice.java
@@ -0,0 +1,11 @@
+package FirstPractice.Head01_JavaProgrammingStart.example06;
+
+public class Practice {
+    public static void main(String[] args){
+        String[] chart = {"A", "B", "C"};
+
+        for(String s : chart){
+            System.out.println(s);
+        }
+    }
+}
