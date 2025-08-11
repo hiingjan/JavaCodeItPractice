53a097be97a01b0276881efd70c3b30572d8bfc0
hiingjan <phjung1216@naver.com>
2025-08-11 11:45:40 +0900

    good

---
+++ src/main/java/FirstPractice/Head02/example02/Order.java
@@ -0,0 +1,26 @@
+package FirstPractice.Head02.example02;
+
+public class Order {
+
+    String customer;
+    String product;
+    int quantity;
+    int price;
+
+    Order(String customer, String product, int quantity, int price){
+        this.customer = customer;
+        this.product = product;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public int getTotalprice(){
+        return quantity * price;
+    }
+
+    public void printOrderSummary(){
+        System.out.println(customer + " " + product + " " + getTotalprice());
+
+    }
+
+}
