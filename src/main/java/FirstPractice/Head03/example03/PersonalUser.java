b70977b0ff569f1173331af90e13d29d964c07b4
hiingjan <phjung1216@naver.com>
2025-08-12 14:34:04 +0900

    dfa

---
+++ src/main/java/FirstPractice/Head03/example03/PersonalUser.java
@@ -0,0 +1,16 @@
+package FirstPractice.Head03.example03;
+
+public class PersonalUser extends User {
+    private String personalEmail;
+
+    public PersonalUser(String userId, String name, String personalEmail) {
+        super(userId, name); // 부모 생성자 호출
+        this.personalEmail = personalEmail;
+    }
+
+    @Override
+    public void printUserInfo() {
+        super.printUserInfo();
+        System.out.println("개인 이메일: " + personalEmail);
+    }
+}
---
+++ src/main/java/FirstPractice/Head03/example03/User.java
@@ -0,0 +1,16 @@
+package FirstPractice.Head03.example03;
+
+public class User {
+    protected String userId;
+    protected String name;
+
+    public User(String userId, String name) {
+        this.userId = userId;
+        this.name = name;
+    }
+
+    public void printUserInfo() {
+        System.out.println("User ID: " + userId);
+        System.out.println("Name: " + name);
+    }
+}
