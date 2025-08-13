bc9b618ec2c5865e1a7583be54e7615c4f83d016
hiingjan <phjung1216@naver.com>
2025-08-13 11:48:02 +0900

    dfd

---
+++ src/main/java/FirstPractice/head04/example02/DipTest.java
@@ -0,0 +1,14 @@
+package FirstPractice.head04.example02;
+
+public class DipTest {
+    public static void main(String[] args) {
+        NotificationSender[] notificationSenders = new NotificationSender[]{
+                new EmailNotificationSender(),
+                new SmsNotificationSender()
+        };
+        for (NotificationSender notificationSender : notificationSenders) {
+            NotificationService notificationService = new NotificationService(notificationSender);
+            notificationService.notify("Hello World");
+        }
+    }
+}
---
+++ src/main/java/FirstPractice/head04/example02/NotificationSender.java
@@ -0,0 +1,6 @@
+package FirstPractice.head04.example02;
+
+public interface NotificationSender {
+    void send(String message);
+}
+
